da595dcc31ba886dcfac00dcd67bd5272c0b1c1f
Mathias ravelojaona <core.autocrlf>
2024-10-29 22:36:08 +0100

    feat: add task args management

--- src/main/java/cypher/tasktracker/runner/core/TaskRunner.java
+++ src/main/java/cypher/tasktracker/runner/core/TaskRunner.java
@@ -1,12 +1,17 @@
 package cypher.tasktracker.runner.core;
 
+import cypher.tasktracker.runner.TaskAddExecution.AddTaskExecutor;
+import cypher.tasktracker.runner.TaskDeleteExecution.TaskDeleteExecutor;
+import cypher.tasktracker.runner.TaskListExecution.TaskListExecutor;
 import cypher.tasktracker.runner.TaskMenuExecution.TaskMenuExecutor;
+import cypher.tasktracker.runner.TaskUpdateExecution.TaskUpdateExecutor;
 import jakarta.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @Component
@@ -14,6 +19,10 @@ public class TaskRunner {
 
     private final AtomicBoolean running = new AtomicBoolean(true);
     private final TaskMenuExecutor taskMenuExecutor;
+    private final TaskListExecutor taskListExecutor;
+    private final AddTaskExecutor addTaskExecutor;
+    private final TaskDeleteExecutor taskDeleteExecutor;
+    private final TaskUpdateExecutor taskUpdateExecutor;
 
     private static Logger LOG = LoggerFactory
             .getLogger(TaskRunner.class);
@@ -21,15 +30,41 @@ public class TaskRunner {
 
     public TaskRunner(
             final ConfigurableApplicationContext context,
-            final TaskMenuExecutor taskMenuExecutor
+            final TaskMenuExecutor taskMenuExecutor,
+            final TaskDeleteExecutor taskDeleteExecutor,
+            final TaskListExecutor taskListExecutor,
+            final TaskUpdateExecutor taskUpdateExecutor,
+            final AddTaskExecutor addTaskExecutor
     ) {
         this.taskMenuExecutor = taskMenuExecutor;
+        this.taskDeleteExecutor = taskDeleteExecutor;
+        this.taskListExecutor = taskListExecutor;
+        this.addTaskExecutor = addTaskExecutor;
+        this.taskUpdateExecutor = taskUpdateExecutor;
     }
 
     public void run(String... args) throws Exception {
         try {
+
+            AbstractTaskExecutor defaultExecutor = this.taskMenuExecutor;
+
+            Map<String, AbstractTaskExecutor> actions = Map.of(
+                    "list", taskListExecutor,
+                    "add", addTaskExecutor,
+                    "delete", taskDeleteExecutor,
+                    "update", taskUpdateExecutor
+            );
+
+
+            if (args.length != 0 && actions.containsKey(args[0])) {
+                defaultExecutor = actions.get(args[0]);
+            }
+
             while (running.get()) {
-                this.taskMenuExecutor.execute();
+                defaultExecutor.execute();
+                if (defaultExecutor != taskMenuExecutor) {
+                    break;
+                }
             }
         } catch (Exception e) {
             LOG.error(e.getMessage());
