a0c225e7b8e4053d844616666b766fd6b93fef87
Mathias ravelojaona <core.autocrlf>
2024-11-17 23:20:35 +0100

    refactor: remove abstraction on taskRepository temporarily

--- src/main/java/cypher/tasktracker/data/database/repositories/TaskRepository.java
+++ src/main/java/cypher/tasktracker/data/database/repositories/TaskRepository.java
@@ -1,8 +1,18 @@
 package cypher.tasktracker.data.database.repositories;
 
-import cypher.tasktracker.data.core.ITaskDataAccess;
 import cypher.tasktracker.data.database.models.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends ITaskDataAccess, JpaRepository<TaskModel, Long> {
+import java.util.Collection;
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<TaskModel, Long> {
+
+    List<TaskModel> findAllByDoneInOrProgressIn(final Collection<Boolean> done, final Collection<Double> progress);
+
+    List<TaskModel> findAllByDoneInAndProgressIn(final Collection<Boolean> done, final Collection<Double> progress);
+
+    List<TaskModel> findAllByDoneInOrProgressNotIn(final Collection<Boolean> done, final Collection<Double> progress);
+
+    TaskModel removeById(Long id);
 }
